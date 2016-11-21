0594d19d63409e895c6bfde393cc8af5c1aa2978
guiworm <dylan.mccrindle@gmail.com>
2016-11-21 11:17:53 -0500

    Initial Commit

---
+++ W4D1---iPhoneAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W4D1---iPhoneAPI
+//
+//  Created by Dylan McCrindle on 2016-11-21.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ W4D1---iPhoneAPI/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  W4D1---iPhoneAPI
+//
+//  Created by Dylan McCrindle on 2016-11-21.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
